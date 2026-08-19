2125837575fe499c314744ecf8d863d9f62555b6
LPS-coder <arthur_l_garcia@estudante.sesisenai.org.br>
2026-08-19 09:11:29 -0300

    Style - Fiz o HTML da parte de cadastrar usuarios

--- public/cadastrar_usuario.php
+++ public/cadastrar_usuario.php
@@ -22,3 +22,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastrar Usuário</title>
+</head>
+<body>
+    <h2>Cadastrar Usuário (RF1)</h2>
+    <?php if ($mensagem): ?><p><?= $mensagem ?></p><?php endif; ?>
+    <form method="POST">
+        <label>Nome:</label><br>
+        <input type="text" name="nome" required><br><br>
+        <label>E-mail:</label><br>
+        <input type="email" name="email" required><br><br>
+        <button type="submit">Salvar</button>
+    </form>
+    <a href="index.php">Voltar</a>
+</body>
+</html>
+
